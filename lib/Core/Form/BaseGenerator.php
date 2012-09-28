c8968ab94e1080ac6d633ddffa94c83843300bc6
Alex <alex@dreiwerken.de>
2012-09-28 10:38:07 +0200

    OptGroups in Selectboxen

--- lib/Core/Form/BaseGenerator.php
+++ lib/Core/Form/BaseGenerator.php
@@ -16,10 +16,10 @@ use Core\Form,
 class BaseGenerator
 {
 	protected $form;
-	
+
 	/**
 	 * Konstruktor
-	 * 
+	 *
 	 * @param string $action Ziel des Formulars
 	 * @param array  $data   Formulardaten
 	 */
@@ -27,63 +27,64 @@ class BaseGenerator
 	{
 		$this->form = new Form($data);
 		$this->form->setAction($action);
-		
+
 	}
-	
+
 	/**
 	 * Liefert das Formularobjekt
-	 * 
+	 *
 	 * @return \Core\Form
 	 */
 	public function getForm()
 	{
 		return $this->form;
-		
+
 	}
-	
+
 	/**
 	 * Liefert das Formularobjekt zur Ausgabe
-	 * 
+	 *
 	 * @return \Core\Form
 	 */
 	public function asString()
 	{
 		return $this->form;
-		
+
 	}
-		
+
 	/**
 	 * Standardvalidierung nach Pflichtfeldern
-	 * 
+	 *
 	 * @param Core\Form|\Core\Form\Element $elementContainer
-	 * 
+	 *
 	 * @return boolean
 	 */
 	protected function checkRequired($elementContainer)
 	{
 		$checkup = true;
-		
+
 		foreach ($elementContainer->getElements() as $el)
 		{
-			$check = $el->validate(); 
-			
+
+			$check = $el->validate();
+
 			if ($check !== true)
 			{
 				SystemMessages::addError($check);
 				$el->addCssClass('error');
 				$checkup = false;
 			}
-			
+
 			if ($el->hasElements())
 			{
 				$check = $this->checkRequired($el);
 				$checkup = $checkup && $check;
 			}
 		}
-		
+
 		return $checkup;
 	}
-	
+
 }
 
 
