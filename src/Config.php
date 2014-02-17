63e11a8b89778e8e68f4eb94ba91bae8a87cce84
Christopher Marchfelder <cm@whm-gmbh.net>
2014-02-17 07:37:31 +0100

    Config Klasse angepasst damit jetzt auch ein host übergeben werden kann. Wird keiner übergeben, versucht die Klasse nach wie vor über den Hostname aus dem HTTP Header aufzulösen.

--- src/Config.php
+++ src/Config.php
@@ -27,6 +27,19 @@ class Config
 	protected $_vars = array();
 
 	/**
+	 * @var string
+	 */
+	protected $_configFolder;
+
+	/**
+	 * Baut den Pfad zur Config auf
+	 */
+	public function __construct()
+	{
+		$this->_configFolder = sprintf('%s/Conf', APPLICATION_PATH);
+	}
+
+	/**
 	 * Fügt die Zuweisung von Hostname zu Config-File dem internen Array zu
 	 *
 	 * @param string $domain   Domainname
@@ -39,52 +52,95 @@ class Config
 	}
 
 	/**
-	 * Lädt die Konfigurationsvariablen und speichert sie zwischen.
-	 * Wenn eine Konfigurationsvariable KEIN Array ist, wird sie als globale Konstante definiert.
+	 * Löst den Hostnamen auf und liefert ihn zurück. Sollte der Hostname nicht gefunden werden, liefert
+	 * die Methode false.
 	 *
-	 * @return void
-	 * @throws \InvalidArgumentException
-	 * @throws \LengthException
+	 * @return string|false
 	 */
-	public function load()
+	protected function resolveHostname()
 	{
-		if (!isset($_SERVER['HTTP_HOST']) || !is_string($_SERVER['HTTP_HOST']))
-		{
-			throw new \InvalidArgumentException('Unbekannter Hostname');
-		}
+		return isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : false;
+	}
 
-		$configsLoaded = 0;
-		$hostname = '.' . trim($_SERVER['HTTP_HOST'], '.');
-		$conf = array();
-		$configurations = array();
+	/**
+	 * Liefert zurück, ob SSL benutzt werden soll
+	 *
+	 * @return bool
+	 */
+	protected function useSSL()
+	{
+		return isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
+	}
 
-		foreach ($this->_domains as $domain => $fileName)
+	/**
+	 * Liefert zurück, ob für diesen Host eine SSL Config existiert
+	 *
+	 * @param string $file
+	 *
+	 * @return bool
+	 */
+	protected function hasSSLConfig($file)
+	{
+		$file = sprintf('%s/%s', $this->_configFolder, $this->getConfigFileWithSSLPattern($file));
+		return file_exists($file);
+	}
+
+	/**
+	 * Liefert den Namen einer SSL Config Datei anhand des Filenamens
+	 *
+	 * @param string $file
+	 *
+	 * @return strinfg
+	 */
+	protected function getConfigFileWithSSLPattern($file)
+	{
+		return sprintf('ssl.%s', $file);
+	}
+
+	/**
+	 * Liefert alle möglichen Config Files zurück
+	 *
+	 * @param string $host
+	 *
+	 * @return string
+	 */
+	protected function seekConfigFiles($host)
+	{
+		$files = array();
+
+		foreach ($this->_domains as $domain => $file)
 		{
-			if ($this->_hostEndsWith($hostname, $domain) || $domain == 'general')
+			if (!$this->_hostEndsWith($host, $domain) && $domain !== 'general')
 			{
-				$configurations[] = $fileName;
-				if (array_key_exists('HTTPS', $_SERVER) && ($_SERVER['HTTPS'] == "on")){
-					if (file_exists(APPLICATION_PATH . '/Conf/ssl.'.$fileName)){
-						$configurations[] = "ssl.".$fileName;
-					}
-				}
+				continue;
+			}
+
+			$files[] = $file;
+			if ($this->useSSL() && $this->hasSSLConfig($file))
+			{
+				$files[] = $this->getConfigFileWithSSLPattern($file);
 			}
 		}
 
-		foreach ($configurations as $fileName)
-		{
-			require APPLICATION_PATH . '/Conf/' . $fileName;
-			\merging($conf, $config);
+		return $files;
+	}
 
-			$configsLoaded++;
-		}
+	/**
+	 * @param $amount
+	 */
+	protected function hasEnoughConfigs($amount)
+	{
+		return $amount >= 2;
+	}
 
-		if ($configsLoaded < 2)
-		{
-			throw new \LengthException('Keine Konfiguration für den Host ' . $hostname . ' gefunden');
-		}
-
-		foreach ($conf as $key => $value)
+	/**
+	 * Erzeugt aus den $configVariables ein Array oder speichert es in das $_vars Array
+	 *
+	 * @param array $configVariables
+	 */
+	protected function makeGlobal(array $configVariables)
+	{
+		foreach ($configVariables as $key => $value)
 		{
 			if (is_scalar($value) && !defined($key))
 			{
@@ -98,6 +154,61 @@ class Config
 	}
 
 	/**
+	 * Bindet alle Configs vom files array ein
+	 *
+	 * @param array $files
+	 */
+	protected function loadConfigFromFS(array $files)
+	{
+		$configVariables = array();
+
+		foreach ($files as $file)
+		{
+			if (!file_exists(sprintf('%s/%s', $this->_configFolder, $file)))
+			{
+				continue;
+			}
+
+			require sprintf('%s/%s', $this->_configFolder, $file);
+			\merging($configVariables, $config);
+		}
+
+		$this->makeGlobal($configVariables);
+	}
+
+	/**
+	 * Lädt die Konfigurationsvariablen und speichert sie zwischen.
+	 * Wenn eine Konfigurationsvariable KEIN Array ist, wird sie als globale Konstante definiert.
+	 *
+	 * @param string $host die host datei die geladen werden soll
+	 *
+	 * @return void
+	 * @throws \InvalidArgumentException
+	 * @throws \LengthException
+	 */
+	public function load($host = false)
+	{
+		if ($host === false) {
+			$host = $this->resolveHostname();
+			if ($host === false) {
+				throw new \InvalidArgumentException('Kein Hostname gefunden');
+			}
+		}
+
+		$host = sprintf('.%s', $host);
+		$host = trim($host, '.');
+
+		// Die Dateien existieren nicht zwangsläufig
+		$files  = $this->seekConfigFiles($host);
+		if (!$this->hasEnoughConfigs(count($files)))
+		{
+			throw new \LengthException('Keine Konfiguration für den Host gefunden');
+		}
+
+		$this->loadConfigFromFS($files);
+	}
+
+	/**
 	 * Prüft ob der aktuelle Hostname mit dem angegebenen übereinstimmt.
 	 *
 	 * @param string $haystack Aktueller Hostname
@@ -106,14 +217,7 @@ class Config
 	 */
 	protected function _hostEndsWith($haystack, $needle)
 	{
-		if (substr($haystack, -strlen($needle)) == $needle)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return \Core\String::endsWith($haystack, $needle);
 	}
 
 	/**
